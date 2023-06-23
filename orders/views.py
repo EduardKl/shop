4f223dadc6493dd8e02291e4592dd6da5d33f015
EduardKl <KlyukvinEd@gmail.com>
2023-06-24 00:05:43 +0300

    Configured the mail client for smtp.yandex. Now, when registering an order, a written confirmation is sent to the client's email.

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
+from shop_site import settings
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -12,14 +14,29 @@ def order_create(request):
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
+            cart_items = []
             for item in cart:
+                product = item['product']
+                price = item['price']
+                quantity = item['quantity']
+
+
+                cart_items.append({'name': product.name, 'total_price': item['total_price'], 'price': price, 'quantity': quantity})
                 OrderItem.objects.create(
                     order = order,
-                    product = item['product'],
-                    price = item['price'],
-                    quantity = item['quantity']
+                    product = product,
+                    price = price,
+                    quantity = quantity
                 )
             cart.clear()
+
+            
+            send_mail(f'Заказ {order.id} оформлен.',
+                      f'Заказ {order.id} на товары {cart_items} оформлен.',
+                      settings.EMAIL_HOST_USER,
+                      [order.email], 
+                      fail_silently=False
+            )
             return render(request, 'orders/order_created.html', {'order': order})
     else:
         form = OrderCreateForm()
